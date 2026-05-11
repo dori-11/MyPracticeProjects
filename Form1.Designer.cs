0e057779f88dec4e298fc26135d54c9ca5adc6c9
dori-11 <matthew0812@naver.com>
2026-05-11 18:03:22 +0900

    feat: initial commit for image viewer

---
+++ Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace ImageViewer
+{
+    partial class label1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picCanvas = new System.Windows.Forms.PictureBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.btnGrayscale = new System.Windows.Forms.Button();
+            this.btnFlip = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnInvert = new System.Windows.Forms.Button();
+            this.tkBrightness = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tkContrast = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tkSaturation = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnRotateR = new System.Windows.Forms.Button();
+            this.btnRotateL = new System.Windows.Forms.Button();
+            this.bthFlip = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tkBrightness)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tkContrast)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tkSaturation)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picCanvas
+            // 
+            this.picCanvas.Location = new System.Drawing.Point(113, 53);
+            this.picCanvas.Name = "picCanvas";
+            this.picCanvas.Size = new System.Drawing.Size(480, 374);
+            this.picCanvas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picCanvas.TabIndex = 0;
+            this.picCanvas.TabStop = false;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(830, 100);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(80, 80);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "이미지 열기\r\n";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(298, 444);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(97, 12);
+            this.lblInfo.TabIndex = 2;
+            this.lblInfo.Text = "선택된 파일 없음";
+            // 
+            // btnGrayscale
+            // 
+            this.btnGrayscale.Location = new System.Drawing.Point(830, 190);
+            this.btnGrayscale.Name = "btnGrayscale";
+            this.btnGrayscale.Size = new System.Drawing.Size(80, 80);
+            this.btnGrayscale.TabIndex = 3;
+            this.btnGrayscale.Text = "흑백전환";
+            this.btnGrayscale.UseVisualStyleBackColor = true;
+            this.btnGrayscale.Click += new System.EventHandler(this.btnGrayscale_Click);
+            // 
+            // btnFlip
+            // 
+            this.btnFlip.Location = new System.Drawing.Point(920, 190);
+            this.btnFlip.Name = "btnFlip";
+            this.btnFlip.Size = new System.Drawing.Size(80, 80);
+            this.btnFlip.TabIndex = 5;
+            this.btnFlip.Text = "좌우 대칭";
+            this.btnFlip.UseVisualStyleBackColor = true;
+            this.btnFlip.Click += new System.EventHandler(this.btnFlip_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(830, 467);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(170, 80);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "저장";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnInvert
+            // 
+            this.btnInvert.Location = new System.Drawing.Point(920, 100);
+            this.btnInvert.Name = "btnInvert";
+            this.btnInvert.Size = new System.Drawing.Size(80, 80);
+            this.btnInvert.TabIndex = 7;
+            this.btnInvert.Text = "색상 반전";
+            this.btnInvert.UseVisualStyleBackColor = true;
+            this.btnInvert.Click += new System.EventHandler(this.btnInvert_Click);
+            // 
+            // tkBrightness
+            // 
+            this.tkBrightness.Location = new System.Drawing.Point(113, 468);
+            this.tkBrightness.Maximum = 100;
+            this.tkBrightness.Minimum = -100;
+            this.tkBrightness.Name = "tkBrightness";
+            this.tkBrightness.Size = new System.Drawing.Size(480, 45);
+            this.tkBrightness.TabIndex = 8;
+            this.tkBrightness.Scroll += new System.EventHandler(this.tkBrightness_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 479);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "밝기 조절";
+            // 
+            // tkContrast
+            // 
+            this.tkContrast.Location = new System.Drawing.Point(113, 494);
+            this.tkContrast.Maximum = 100;
+            this.tkContrast.Minimum = -100;
+            this.tkContrast.Name = "tkContrast";
+            this.tkContrast.Size = new System.Drawing.Size(480, 45);
+            this.tkContrast.TabIndex = 10;
+            this.tkContrast.Scroll += new System.EventHandler(this.tkContrast_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 501);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 12);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "대비 조절";
+            // 
+            // tkSaturation
+            // 
+            this.tkSaturation.Location = new System.Drawing.Point(113, 519);
+            this.tkSaturation.Maximum = 200;
+            this.tkSaturation.Name = "tkSaturation";
+            this.tkSaturation.Size = new System.Drawing.Size(480, 45);
+            this.tkSaturation.TabIndex = 12;
+            this.tkSaturation.Value = 100;
+            this.tkSaturation.Scroll += new System.EventHandler(this.tkSaturation_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(59, 527);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 12);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "채도 조절";
+            // 
+            // btnRotateR
+            // 
+            this.btnRotateR.Location = new System.Drawing.Point(920, 280);
+            this.btnRotateR.Name = "btnRotateR";
+            this.btnRotateR.Size = new System.Drawing.Size(80, 80);
+            this.btnRotateR.TabIndex = 14;
+            this.btnRotateR.Text = "90도 우회전";
+            this.btnRotateR.UseVisualStyleBackColor = true;
+            this.btnRotateR.Click += new System.EventHandler(this.btnRotateR_Click);
+            // 
+            // btnRotateL
+            // 
+            this.btnRotateL.Location = new System.Drawing.Point(830, 280);
+            this.btnRotateL.Name = "btnRotateL";
+            this.btnRotateL.Size = new System.Drawing.Size(80, 80);
+            this.btnRotateL.TabIndex = 15;
+            this.btnRotateL.Text = "90도 좌회전";
+            this.btnRotateL.UseVisualStyleBackColor = true;
+            this.btnRotateL.Click += new System.EventHandler(this.btnRotateL_Click);
+            // 
+            // bthFlip
+            // 
+            this.bthFlip.Location = new System.Drawing.Point(830, 370);
+            this.bthFlip.Name = "bthFlip";
+            this.bthFlip.Size = new System.Drawing.Size(80, 80);
+            this.bthFlip.TabIndex = 16;
+            this.bthFlip.Text = "좌우 반전";
+            this.bthFlip.UseVisualStyleBackColor = true;
+            this.bthFlip.Click += new System.EventHandler(this.bthFlip_Click);
+            // 
+            // label1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 661);
+            this.Controls.Add(this.bthFlip);
+            this.Controls.Add(this.btnRotateL);
+            this.Controls.Add(this.btnRotateR);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tkSaturation);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tkContrast);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tkBrightness);
+            this.Controls.Add(this.btnInvert);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnFlip);
+            this.Controls.Add(this.btnGrayscale);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.picCanvas);
+            this.Name = "label1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tkBrightness)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tkContrast)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tkSaturation)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picCanvas;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Button btnGrayscale;
+        private System.Windows.Forms.Button btnFlip;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnInvert;
+        private System.Windows.Forms.TrackBar tkBrightness;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar tkContrast;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar tkSaturation;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnRotateR;
+        private System.Windows.Forms.Button btnRotateL;
+        private System.Windows.Forms.Button bthFlip;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageViewer
+{
+    public partial class label1 : Form
+    {
+        Bitmap originBmp;
+        public label1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog()) // 파일 창 생성
+            {
+                ofd.Filter = "이미지 파일|*.jpg;*.jpeg;*.png;*.bmp"; // 확장자 필터링 (엉뚱한 파일 못 고르게 막기)
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    using (Image tempImg = Image.FromFile(ofd.FileName))
+                    {
+                        picCanvas.Image = new Bitmap(tempImg);
+                    }
+                    // 사진 불러오기
+                    // [중요] 나중에 수정을 하려면 파일에서 직접 불러오는 것보다 
+                    // 새로운 Bitmap 객체로 복사해서 메모리에 올리는 게 안전해.
+                    Image img = Image.FromFile(ofd.FileName);
+
+                    lblInfo.Text = $"파일명: {ofd.SafeFileName} ({img.Width} x {img.Height})"; // 정보표시
+
+                    img.Dispose(); //사용한 원본 파일 자원은 바로 해제
+                }
+            }
+            originBmp = new Bitmap(picCanvas.Image);
+        }
+
+        private void btnGrayscale_Click(object sender, EventArgs e)
+        {
+            if (picCanvas.Image == null) return;
+
+            Bitmap oldBitmap = new Bitmap(picCanvas.Image);
+
+            Bitmap newBitmap = new Bitmap(oldBitmap.Width, oldBitmap.Height);
+
+            for (int y = 0; y < oldBitmap.Height; y++)
+            {
+                for (int x = 0; x < oldBitmap.Width; x++)
+                {
+                    Color c = oldBitmap.GetPixel(x, y);
+
+                    int gray = (int)((c.R + c.G + c.B) / 3);
+
+                    Color grayColor = Color.FromArgb(gray, gray, gray);
+
+                    newBitmap.SetPixel(x, y, grayColor);
+                }
+            }
+            picCanvas.Image = newBitmap;
+        }
+
+        private void btnInvert_Click(object sender, EventArgs e)
+        {
+            if (picCanvas.Image == null) return;
+
+            // 1. 원본 이미지를 복사해서 Bitmap 객체 생성
+            Bitmap bmp = new Bitmap(picCanvas.Image);
+            Bitmap newBmp = new Bitmap(bmp.Width, bmp.Height);
+
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    Color c = bmp.GetPixel(x, y);
+
+                    // 반전 로직 (정확함!)
+                    int r = 255 - c.R;
+                    int g = 255 - c.G;
+                    int b = 255 - c.B;
+
+                    newBmp.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+
+            // 2. 기존 이미지 메모리 해제 
+            if (picCanvas.Image != null) picCanvas.Image.Dispose();
+
+            // 3. 새 이미지 할당 및 갱신
+            picCanvas.Image = newBmp;
+            picCanvas.Refresh(); 
+        }
+
+        private void btnFlip_Click(object sender, EventArgs e)
+        {
+            if (picCanvas.Image == null) return; // 사진이 없으면 계산 시작 하지 마셈.
+
+            Bitmap bmp = (Bitmap)picCanvas.Image; // 현재 화면의 이미지를 비트맵으로 준비
+
+            Bitmap newBmp = new Bitmap(bmp.Width, bmp.Height); // 결과물을 담을 똑같은 빈 비트맵 준비
+
+            for (int y = 0; y < bmp.Height; y++) // 모든 픽셀 하나씩 지나가기
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    Color c = bmp.GetPixel(x, y); // 한 점의 픽셀에서 색 확인
+
+                    int targetX = (bmp.Width - 1) - x; // 대칭좌표 계산. 공식 (전체 너비 -1) - 현재위치
+
+                    newBmp.SetPixel(targetX, y, c); // 반대편에 찍기.
+                }
+            }
+            picCanvas.Image = newBmp; // 완성된 사진을 불러와.
+
+            // -1을 하는 이유는 컴퓨터는 0부터 세기 때문에 오류 방지.
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (picCanvas.Image == null) return;
+
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Title = "편집한 이미지 저장하기";
+                sfd.FileName = "ResultImage";
+                sfd.Filter = "PNG Image|*.png|JPG Image|*.jpg|Bitmap Image|*.bmp";
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    string ext = System.IO.Path.GetExtension(sfd.FileName).ToLower();
+                    if (ext == ".jpg" || ext == ".jpeg")
+                        picCanvas.Image.Save(sfd.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    else if (ext == ".png")
+                        picCanvas.Image.Save(sfd.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                    else
+                        picCanvas.Image.Save(sfd.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                    MessageBox.Show("성공적으로 저장되었습니다.");
+                }
+            }
+        }
+
+        private void tkBrightness_Scroll(object sender, EventArgs e)
+        {
+            if (originBmp == null) return; // 원본이 없으면 실행 안 함
+
+            // 항상 원본에서 새로 시작함!
+            Bitmap newBmp = new Bitmap(originBmp.Width, originBmp.Height);
+
+            int val = tkBrightness.Value;
+
+            for (int y = 0; y < originBmp.Height; y++)
+            {
+                for (int x = 0; x < originBmp.Width; x++)
+                {
+                    Color c = originBmp.GetPixel(x, y); // 원본에서 픽셀 가져오기
+
+                    // ... (밝기 연산 및 클램핑 로직은 그대로) ...
+                    int r = Math.Max(0, Math.Min(255, c.R + val));
+                    int g = Math.Max(0, Math.Min(255, c.G + val));
+                    int b = Math.Max(0, Math.Min(255, c.B + val));
+
+                    newBmp.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+
+            // 이전 이미지 메모리 해제 후 화면 갱신
+            if (picCanvas.Image != null) picCanvas.Image.Dispose();
+            picCanvas.Image = newBmp;
+        }
+
+        private void tkContrast_Scroll(object sender, EventArgs e)
+        {
+            if (originBmp == null) return; // 원본 없으면 실행 안함
+
+            Bitmap newBmp = new Bitmap(originBmp.Width, originBmp.Height); // 빈 비트맵 만들기
+
+            double contrast = Math.Pow((100.0 + tkContrast.Value) / 100.0, 2); // 대비 강도 조절 공식
+
+            // 모든 픽셀을 하나씩 방문
+
+            for (int y = 0; y < originBmp.Height; y++)
+            {
+                for (int x = 0; x < originBmp.Width; x++)
+                {
+                    Color c = originBmp.GetPixel(x, y); // 픽셀 불러오기
+
+                    // 색 대비 공식
+
+                    double r = ((((c.R / 255.0) - 0.5) * contrast) + 0.5) * 255.0;
+                    double g = ((((c.G / 255.0) - 0.5) * contrast) + 0.5) * 255.0;
+                    double b = ((((c.B / 255.0) - 0.5) * contrast) + 0.5) * 255.0;
+
+                    // 클램핑 적용. 연산 결과가 0보다 작으면 0, 255보다 크면 255로 고정
+
+                    int finalR = Math.Max(0, Math.Min(255, (int)r));
+                    int finalG = Math.Max(0, Math.Min(255, (int)g));
+                    int finalB = Math.Max(0, Math.Min(255, (int)b));
+
+                    newBmp.SetPixel(x, y, Color.FromArgb(finalR, finalG, finalB));  // 새 비트맵에 계산 결과 적용
+                }
+            }
+
+            // 기존 메모리 정리 후 새 이미지 출력
+            if (picCanvas.Image != null)picCanvas.Image.Dispose(); 
+            picCanvas.Image = newBmp;
+        }
+
+        private void tkSaturation_Scroll(object sender, EventArgs e)
+        {
+            if (originBmp == null) return; // 사진이 없으면 계산하지 말기.
+
+            Bitmap newBmp = new Bitmap(originBmp.Width, originBmp.Height); // 새로운 비트맵 만들기
+
+            double saturation = tkSaturation.Value / 100.0; // 채도 계수
+
+            // 픽셀 하나씩 방문하기
+
+            for(int y = 0; y < originBmp.Height; y++)
+            {
+                for (int x = 0; x < originBmp.Width; x++)
+                {
+                    Color c = originBmp.GetPixel(x, y); // 픽셀 도착
+
+                    // 평균 밝기(무채색) 계산
+
+                    int gray = (c.R + c.G + c.B) / 3;
+
+                    // 채도 공식 적용
+
+                    double r = gray + (c.R - gray) * saturation;
+                    double g = gray + (c.G - gray) * saturation;
+                    double b = gray + (c.B - gray) * saturation;
+
+                    // 클램핑 (역시나 0보다 작으면 0, 255보다 크면 255로 고정)
+
+                    int finalR = Math.Max(0, Math.Min(255, (int)r));
+                    int finalG = Math.Max(0, Math.Min(255, (int)g));
+                    int finalB = Math.Max(0, Math.Min(255, (int)b));
+
+                    // 계산된 결과 비트맵에 찍기
+
+                    newBmp.SetPixel(x, y, Color.FromArgb(finalR, finalG, finalB));
+                }
+            }
+
+            // 화면 갱신
+
+            if (picCanvas.Image != null) picCanvas.Image.Dispose();
+            picCanvas.Image = newBmp;
+        }
+
+        private void btnRotateR_Click(object sender, EventArgs e)
+        {
+            if (picCanvas.Image == null) return;
+
+            Bitmap oldBmp = new Bitmap(picCanvas.Image);
+            Bitmap newBmp = new Bitmap(oldBmp.Height, oldBmp.Width);
+
+            for (int y = 0; y < oldBmp.Height; y++)
+            {
+                for (int x = 0; x < oldBmp.Width; x++)
+                {
+                    Color c = oldBmp.GetPixel(x, y);
+
+                    int newX = (oldBmp.Height - 1) - y;
+                    int newY = x;
+
+                    newBmp.SetPixel(newX, newY, c);
+                }
+            }
+            picCanvas.Image = newBmp;
+
+            if (originBmp != null) originBmp.Dispose();
+            originBmp = new Bitmap(newBmp);
+
+            oldBmp.Dispose();
+        }
+
+        private void btnRotateL_Click(object sender, EventArgs e)
+        {
+            if (picCanvas.Image == null) return; // 새 비트맵 준비
+
+            // 비트맵과 같은 크기의 비트맵 준비
+
+            Bitmap oldBmp = new Bitmap(picCanvas.Image);
+            Bitmap newBmp = new Bitmap(oldBmp.Height, oldBmp.Width);
+
+            // 픽셀 하나 씩 방문
+
+            for (int y = 0; y < oldBmp.Height; y++)
+            {
+                for (int x = 0; x < oldBmp.Width; x++)
+                {
+                    Color c = oldBmp.GetPixel(x, y);
+
+                    int newX = y;
+                    int newY = (oldBmp.Width - 1) - x;
+
+                    newBmp.SetPixel(newX, newY, c);
+                }
+            }
+            picCanvas.Image = newBmp;
+
+            if (originBmp != null) originBmp.Dispose();
+            originBmp = new Bitmap(newBmp);
+
+            oldBmp.Dispose();
+        }
+
+        private void bthFlip_Click(object sender, EventArgs e)
+        {
+            if (picCanvas == null) return; // 도화지 준비
+
+            // 가로세로 크기가 똑같은 도화지 하나 더
+
+            Bitmap oldBmp = new Bitmap(picCanvas.Image);
+            Bitmap newBmp = new Bitmap(oldBmp.Width, oldBmp.Height);
+
+            // 픽셀 하나 씩 방문
+
+            for (int y = 0; y < oldBmp.Height; y++)
+            {
+                for(int x = 0; x < oldBmp.Width; x++)
+                {
+                    Color c = oldBmp.GetPixel(x, y); // 픽셀이 도착헀을 때
+
+                    // 좌우 반전 공식
+
+                    int newX = (oldBmp.Width - 1) - x;
+                    int newY = y;
+
+                    newBmp.SetPixel(newX, newY, c); // 새 도화지에 픽셀 찍기
+                }
+            }
+            // 초기화
+
+            picCanvas.Image = newBmp;
+
+            if(originBmp != null) originBmp.Dispose();
+            originBmp = new Bitmap(newBmp);
+
+            oldBmp.Dispose();
+        }
+    }
+}
